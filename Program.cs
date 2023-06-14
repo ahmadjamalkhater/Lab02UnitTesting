6ed0fe77ec66b24ae8799928899e4642abeb560a
ahmad jamal <ahmadjamalkhater@gmail.com>
2023-06-14 18:57:55 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab02UnitTesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
