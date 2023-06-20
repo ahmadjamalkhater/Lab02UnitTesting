ed7a6813cb0aaaccac7fa00047a1d8f6e6a42dd5
ahmad jamal <ahmadjamalkhater@gmail.com>
2023-06-20 23:53:54 +0300

    Upload after storage

--- Program.cs
+++ Program.cs
@@ -1,46 +1,109 @@
 ﻿using System;
-using System.Collections.Generic;
 
-namespace Lab02UnitTesting
+public class ATM
 {
-    public class Program
+    public decimal Balance { get; set; }
+
+    public decimal ViewBalance()
     {
-        public static decimal Balance { get; set; }
-        public static List<string> TransactionHistory { get; set; } = new List<string>();
+        return Balance;
+    }
 
-        static void Main(string[] args)
+    public decimal Withdraw(decimal amount)
+    {
+        if (amount > 0 && amount <= Balance)
         {
-            // Application logic goes here
+            Balance -= amount;
+            return amount;
         }
 
-        public static decimal ViewBalance()
+        return 0;
+    }
+
+    public decimal Deposit(decimal amount)
+    {
+        if (amount > 0)
         {
-            return Balance;
+            Balance += amount;
+            return amount;
         }
 
-        public static decimal Withdraw(decimal amount)
+        return 0;
+    }
+
+    public static void Main()
+    {
+        UserInterface();
+    }
+
+    public static void UserInterface()
+    {
+        ATM atm = new ATM();
+
+        while (true)
         {
-            if (amount > 0 && amount <= Balance)
+            Console.WriteLine("ATM Options:");
+            Console.WriteLine("1. View Balance");
+            Console.WriteLine("2. Withdraw");
+            Console.WriteLine("3. Deposit");
+            Console.WriteLine("4. Exit");
+            Console.Write("Enter your choice: ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
             {
-                Balance -= amount;
-                TransactionHistory.Add($"Withdrawal of {amount:C}. New Balance: {Balance:C}");
+                case "1":
+                    Console.WriteLine($"Current balance: {atm.ViewBalance():C}");
+                    break;
+                case "2":
+                    Console.Write("Enter withdrawal amount: ");
+                    decimal withdrawalAmount;
+                    if (decimal.TryParse(Console.ReadLine(), out withdrawalAmount))
+                    {
+                        decimal withdrawnAmount = atm.Withdraw(withdrawalAmount);
+                        if (withdrawnAmount > 0)
+                        {
+                            Console.WriteLine($"Successfully withdrew {withdrawnAmount:C}. Current balance: {atm.ViewBalance():C}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid withdrawal amount or insufficient funds.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid withdrawal amount. Please enter a valid number.");
+                    }
+                    break;
+                case "3":
+                    Console.Write("Enter deposit amount: ");
+                    decimal depositAmount;
+                    if (decimal.TryParse(Console.ReadLine(), out depositAmount))
+                    {
+                        decimal depositedAmount = atm.Deposit(depositAmount);
+                        if (depositedAmount > 0)
+                        {
+                            Console.WriteLine($"Successfully deposited {depositedAmount:C}. Current balance: {atm.ViewBalance():C}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid deposit amount.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid deposit amount. Please enter a valid number.");
+                    }
+                    break;
+                case "4":
+                    Console.WriteLine("Exiting the application...");
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice. Please enter a valid option.");
+                    break;
             }
-            return Balance;
-        }
-
-        public static decimal Deposit(decimal amount)
-        {
-            if (amount > 0)
-            {
-                Balance += amount;
-                TransactionHistory.Add($"Deposit of {amount:C}. New Balance: {Balance:C}");
-            }
-            return Balance;
-        }
-
-        public static List<string> GetTransactionHistory()
-        {
-            return TransactionHistory;
+            Console.WriteLine();
         }
     }
 }
